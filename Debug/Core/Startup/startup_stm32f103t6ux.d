f70587592aec9363ae95edfb66a1c3f85ec7aeca
v1Vd0unA1 <gouldwang@gmail.com>
2026-04-19 00:59:11 +0800

    Add conservative gitignore

--- Debug/Core/Startup/startup_stm32f103t6ux.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32f103t6ux.o: \
- ../Core/Startup/startup_stm32f103t6ux.s
--- Debug/Drivers/BSP/TIME/app_beijing_time.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/TIME/app_beijing_time.o: \
- ../Drivers/BSP/TIME/app_beijing_time.c \
- ../Drivers/BSP/TIME/app_beijing_time.h
-../Drivers/BSP/TIME/app_beijing_time.h:
